7c894278577f4602d5cf84f1fd0da804ce6a7c4c
Võ Trường <truong.vd2000@gmail.com>
2024-09-12 13:19:37 +0700

    first commit

---
+++ core/settings.php
@@ -0,0 +1,16 @@
+<?php
+# MySQL db host name
+$sql_db_host = "127.0.0.1";
+
+# MySQL db User
+$sql_db_user = "root";
+
+# MySQL db Password
+$sql_db_pass = "truongvo1503";
+
+# MySQL db Name
+$sql_db_name = "dotdb";
+
+# Site URL
+$site_url    = "http://localhost/dot"; // E.g (https://www.colibrism.com)
+?>
